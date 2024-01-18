079d0617a4d6a13889da4a55545ad63ba37d91c5
Cheko <156435935+Cheko82@users.noreply.github.com>
2024-01-18 12:02:30 +0530

    Update: main.py fix function names

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_world():
+def home():
     return render_template("index.html")
 
+
 @app.route("/todo")
-def hello_world():
+def todo():
     return render_template("todo.html")
+
 
 app.run(debug=True)
