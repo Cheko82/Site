b94222916f89bf3042bad948889856328c103393
Cheko82 <jijdev@proton.me>
2024-01-18 17:49:52 +0530

    Update: Edited colors, made stuff responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ["./templates/**/*.{html,js}"],
   theme: {
+    colors: {
+      neutral:        '#0a0a0a',
+      foreground:     '#84b584',
+      background:     '#171717',
+      backgroundAlt:  '#27272a',
+      primary:        '#baffba',
+      secondary:      '#536b4e',
+      disabled:       '#3f503c',
+    },
     extend: {
       fontFamily: {
         Pacifico: ["Pacifico", "sans-serif"],
