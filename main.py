5498a9a247c93797beb295ebfa3c27923c0fda32
Cheko <156435935+Cheko82@users.noreply.github.com>
2024-01-19 13:20:24 +0530

    Update: main.py Added blog

--- main.py
+++ main.py
@@ -14,12 +14,23 @@ def todo():
     return render_template("todo.html")
 
 
-@app.route("/blog")
+@app.route("/blogs")
 def blog():
-    with open("./postlist.json", "r") as file:
-        postlist = [l for l in json.load(file)]
 
-    return render_template("blog.html", postlist=postlist)
+    with open("./postlist.json", "r") as file:
+        blogs = json.load(file)
+
+    return render_template("blog.html", blogs=blogs)
+
+
+@app.route('/blogs/<title>')
+def blogpost(title):
+    # Load blogs from the JSON file
+    with open('postlist.json', 'r') as file:
+        blogs = json.load(file)
+
+    # blog = blogs[title]
+    return render_template(f'blogs/{title}.html', blog=blog)
 
 
 
